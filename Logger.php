5e7ed02a7417cceb16977056f2d0f89bb28f0937
LAPTOP-FCQICFHN\Simon Daum <s.daum@lackner-media.at>
2017-11-13 14:41:03 +0100

    fix

--- Logger.php
+++ Logger.php
@@ -2,30 +2,33 @@
 
 /**
  * Class Logger
- *
+ * 
  * Super Simple Logging
  */
-define("FILE_PATH", "components/com_immobilien/controllers/log.txt");
+
+define("FILE_PATH", "log.txt");
+
 class Logger
 {
 	private $file;
+	
 	public function __construct()
 	{
 		try {
 			$this->file = fopen(FILE_PATH, 'ab');
 		} catch (Exception $e) {
 			$this->file = null;
-
 		}
-	}
+    }
+	
 	public function log($msg)
 	{
 		fwrite($this->file, "[".date('Y-m-d h:i:s')."] \n" . $msg);
-		printf('<br> \t Wrote %d bytes to %s', $msg, realpath('components/com_immobilien/controllers/log.txt'));
+		printf('<br> \t Wrote %d bytes to %s', $msg, realpath(FILE_PATH));
 	}
+	
 	public function __destruct()
 	{
-
 		fclose($this->file); 
 	}
 }
